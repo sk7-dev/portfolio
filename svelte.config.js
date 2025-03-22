0b18d1df9fb8115e3e64ee470b8964039e339452
sk7-dev <karthee.shiv@gmail.com>
2025-03-22 02:29:46 -0700

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const base = '/portfolio';
+//const base = '/portfolio';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
